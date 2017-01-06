c6bbe38debcf6ccb1bffe88b3a3a7f8f2488ac59
Acer Aspire <me@aceraspire.com>
2017-01-06 23:48:22 +0530

    completed addSyllabus

--- src/app/admin/academics/syllabus/newsyllabus/newsyllabus.component.ts
+++ src/app/admin/academics/syllabus/newsyllabus/newsyllabus.component.ts
@@ -8,43 +8,48 @@ import { DatabaseService } from 'app/services/database.service';
   styles: []
 })
 export class NewsyllabusComponent implements OnInit {
+  ifsuccess: boolean = false;
   newSyllabusForm: FormGroup;
   constructor(private database: DatabaseService) { }
 
   ngOnInit() {
-    this.database.response.subscribe(
-    data => console.log(data)
-    );
+    this.database.response.subscribe(data => this.ifsuccess = data.Success);
     this.newSyllabusForm = new FormGroup({
       'syllabus': new FormGroup({
-        'standard': new FormControl('standard'),
-        'syllabusname': new FormControl('syllabusname')
+        'standard': new FormControl('', Validators.required),
+        'syllabusname': new FormControl('', Validators.required)
       }),
       'subjects': new FormArray([
         new FormGroup({
-        'subjectname': new FormControl('subjectname', Validators.required),
+        'subjectname': new FormControl('', Validators.required),
         'subject-parts': new FormArray([])
         })
       ])
     });
   }
-
   addNewSubject(){
     (<FormArray>this.newSyllabusForm.controls['subjects']).push( new FormGroup({
         'subjectname': new FormControl('', Validators.required),
         'subject-parts': new FormArray([])
         }));
-    console.log(this.newSyllabusForm.controls['subjects'])
   }
-
-  addNewSubjectPart(index){/*
-    (<FormArray>this.newSyllabusForm.controls['subjects'].get(index).controls['subject-parts']).push( new FormGroup({
-        'subjectname': new FormControl('', Validators.required),
-        'subject-parts': new FormArray([])
-        }));*/
+  addNewSubjectPart(index:number){
+    (<FormArray>(<FormArray>(<FormArray>this.newSyllabusForm.controls['subjects'])
+    .controls[index]).controls['subject-parts'])
+    .push(new FormControl('', Validators.required));
   }
-
+  removeSubjectPart(index1:number,index2:number) {
+    (<FormArray>(<FormArray>(<FormArray>this.newSyllabusForm.controls['subjects'])
+    .controls[index1]).controls['subject-parts'])
+    .removeAt(index2);
+  }
+  removeSubject(index:number) {
+    if(index > 0){
+    (<FormArray>this.newSyllabusForm.controls['subjects'])
+    .removeAt(index);
+    }
+  }
   onSubmit(){
-    console.log(this.newSyllabusForm);
+    this.database.query({goto:"addsyllabus",data: this.newSyllabusForm.value});
   }
 }
